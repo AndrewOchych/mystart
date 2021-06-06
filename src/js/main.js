d4cc3440d64f54ae6ce05025c4a0cbd46c82d144
AndrewOchych <ochychandrew@gmail.com>
2021-06-06 19:41:28 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+//JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    // Меню
+    function headerMenuActive() {
+        this.classList.toggle("active");
+    }
+    document
+        .querySelector("#header-menu__icon")
+        .addEventListener("click", headerMenuActive);
+
+
+})
+
+
+
